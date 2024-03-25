cc876d394d6535dacba108a18d849991762aa00e
Raymond Zheng <raymondzheng1994@gmail.com>
2024-03-25 16:16:55 -0400

    first commit

---
+++ hw3.py
@@ -0,0 +1,11 @@
+def what_temperature():
+    while True:
+        try:
+            fahrenheit = float(input("Please enter the temperature in Fahrenheit: "))
+            celsius = (fahrenheit - 32)* 5 / 9
+            print(f"The temperature in celsius is: {celsius}")
+            break
+        except ValueError:
+            print("Not a valid input, please try again using a number")
+print("Thank you for trying out my app!!")
+what_temperature()
